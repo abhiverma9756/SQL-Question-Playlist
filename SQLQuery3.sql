ffbf81a23b7fcadb217e0150c0fc5917a1b23106
ABHISHEK KUMAR <ABHISHEKKUMAR0720056@GMAIL.COM>
2026-08-25 23:59:43 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,18 @@
+-- select distinct(employeeid)
+-- from employee
+
+
+--  select count(lastname) as lastname
+--  from employee
+
+--select max(salary) as maximumm
+--from employeesalary
+
+--select min(salary) as minimumm
+--from employeesalary
+
+--select avg(salary) as average
+--from employeesalary
+
+select * from employee
+where age  >= 30
