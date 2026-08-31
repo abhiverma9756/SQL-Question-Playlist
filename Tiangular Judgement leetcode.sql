2ab4ee35f5dc66a8152cb96b77496ff2f1242158
ABHISHEK KUMAR <ABHISHEKKUMAR0720056@GMAIL.COM>
2026-08-31 22:13:32 +0530

    Implement triangle formation check in SQL
    Added a SQL query to determine if three line segments can form a triangle.

--- Tiangular Judgement leetcode.sql
+++ Tiangular Judgement leetcode.sql
@@ -1,3 +1,6 @@
+Report for every three line segments whether they can form a triangle.
+
+
 select x,y,z,
 case when x+y > z and y+z >x and x+z > y then 'Yes' else 'No' end as triangle
 from triangle
