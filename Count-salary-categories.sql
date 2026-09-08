ca3c16e56cc6ee99b1df5f3ea7dcd5120b77ca70
ABHISHEK KUMAR <ABHISHEKKUMAR0720056@GMAIL.COM>
2026-09-08 23:26:24 +0530

    Fix comment syntax in Count-salary-categories.sql

--- Count-salary-categories.sql
+++ Count-salary-categories.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+'''Write your MySQL query statement below'''
 select "Low Salary" as category ,
 sum(if(income < 20000,1,0)) as accounts_count
 from accounts
