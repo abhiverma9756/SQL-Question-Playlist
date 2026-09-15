0dcf057fb6fb475aaafc8ee0b25f0a998a33b004
ABHISHEK KUMAR <ABHISHEKKUMAR0720056@GMAIL.COM>
2026-09-15 22:08:19 +0530

    Add docstring to letterCombinations method
    Added a docstring to describe the function's purpose.

--- Letter Combination of a Phone Number.py
+++ Letter Combination of a Phone Number.py
@@ -1,3 +1,5 @@
+""Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.""
+
 class Solution:
     def letterCombinations(self, digits):
         if not digits:
